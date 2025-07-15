272f9e26b8451a3bd267333366fa6dd70111c858
Nhat <97539645+aozoramoew@users.noreply.github.com>
2025-07-15 23:35:10 +0700

    Update train.py
    Signed-off-by: Nhat <97539645+aozoramoew@users.noreply.github.com>

--- scripts/reinforcement_learning/skrl/train.py
+++ scripts/reinforcement_learning/skrl/train.py
@@ -117,7 +117,8 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
     env_cfg.scene.num_envs = args_cli.num_envs if args_cli.num_envs is not None else env_cfg.scene.num_envs
     env_cfg.sim.device = args_cli.device if args_cli.device is not None else env_cfg.sim.device
     
-    writer = SummaryWriter(log_dir='./logs')
+    writer = SummaryWriter(log_dir='/root/.nvidia-omniverse/logs')
+
 
     # multi-gpu training config
     if args_cli.distributed:
